0430156f7d083ead803811c2bb3409ea746b3bed
Sheetal R. Dash <dash.sheetal13@gmail.com>
2018-08-08 20:40:29 +0100

    7th August Commit

--- consumer-ipdensity.py
+++ consumer-ipdensity.py
@@ -1,6 +1,6 @@
 import logging
 import sys
-import os, pulsar
+import os, pulsar, mysql.connector
 
 SNIFFER_TOPIC = 'persistent://sample/standalone/ns1/ip_sniffer'
 SUBSCRIPTION = 'sub'
@@ -8,6 +8,22 @@ TIMEOUT = 10000
 # Setup up basic logging
 logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s', level=logging.DEBUG)
 
+def insert_db(source,dest):
+    mydb=mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="mysql",
+        database="cids"
+    )
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO ip_sniffer (source_ip, dest_ip) VALUES (%s, %s)"
+    val = (source, dest)
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    print(mycursor.rowcount, "record inserted.")
 
 def main(args):
     logging.info('Connecting to Pulsar...')
@@ -21,6 +37,7 @@ def main(args):
         try:
             # try and receive messages with a timeout of 10 seconds
             msg = consumer.receive(timeout_millis=TIMEOUT)
+            insert_db(msg.data().split("|")[0],msg.data().split("|")[1])
             logging.info("Received message '%s'", msg.data())
     	    consumer.acknowledge(msg)  # send ack to pulsar for message consumption
         except Exception:
--- sniff-traffic.py
+++ sniff-traffic.py
@@ -1,25 +1,18 @@
 from scapy.all import *
-import pulsar
+import pulsar, commands
+from multiprocessing import Process
 
-def pulsar_publish(message):
-	SNIFFER_TOPIC = 'persistent://sample/standalone/ns1/ip_sniffer'
-
-	# Setup for basic logging
-	logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s', level=logging.INFO)
-	logging.info('Connecting to Pulsar...')
-
-	# Create a pulsar client instance with reference to the broker
-	client = pulsar.Client('pulsar://localhost:6650')
-
-	# Build a producer instance on a specific topic
-	producer = client.create_producer(SNIFFER_TOPIC)
-	logging.info('Connected to Pulsar')
-
-	logging.info('Source|Target: %s ', message)
-	producer.send(message);
-
-
-	client.close()
+SNIFFER_TOPIC = 'persistent://sample/standalone/ns1/ip_sniffer'
+# Setup for basic logging
+logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s', level=logging.INFO)
+logging.info('Connecting to Pulsar...')
+# Create a pulsar client instance with reference to the broker
+client = pulsar.Client('pulsar://localhost:6650')
+# Build a producer instance on a specific topic
+producer = client.create_producer(SNIFFER_TOPIC, max_pending_messages=100000)
+logging.info('Connected to Pulsar')
+def callBack(res, msg):
+	print('Message published: %s'%res)
 
 def sniffPackets(packet):           # custom custom packet sniffer action method
     if not (packet.haslayer(ICMP)):
@@ -28,19 +21,26 @@ def sniffPackets(packet):           # custom custom packet sniffer action method
             pckt_dst=packet[IP].dst
             pckt_ttl=packet[IP].ttl
             print "IP Packet: %s is going to %s and has ttl value %s" % (pckt_src,pckt_dst,pckt_ttl)
-            pulsar_publish(pckt_src+"|"+pckt_dst)
+            producer.send_async(pckt_src + "|" + pckt_dst,callBack)
             return
     elif packet.haslayer(ICMP) and packet[ICMP].type != 0:
         pckt_src=packet[IP].src
         pckt_dst=packet[IP].dst
         pckt_ttl=packet[IP].ttl
         print "IP Packet: %s is going to %s and has ttl value %s" % (pckt_src,pckt_dst,pckt_ttl)
-        pulsar_publish(pckt_src + "|" + pckt_dst)
+	logging.info('Source|Target: %s ', pckt_src + "|" + pckt_dst)
+	producer.send_async(pckt_src + "|" + pckt_dst,callBack)
         return
+def sniffPacketsSwitch(switch):
+	sniff(filter="ip",iface=switch,prn=sniffPackets)
 
 def main():
-	print "custom packet sniffer"
-	sniff(filter="ip",iface="s1-eth1",prn=sniffPackets)
 
+	print "custom packet sniffer"
+	switches=commands.getoutput('ifconfig|egrep "s?-eth*"|awk -F":" \'{print $1}\'')
+	for switch in switches.splitlines():
+		if switch != '0':
+			p=Process(target=sniffPacketsSwitch, args=(switch,))
+			p.start()
 if __name__ == '__main__':
 	main()
