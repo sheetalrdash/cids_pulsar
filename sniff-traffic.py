ecb5ebefb700bb012860c576c5126d76c3c3f843
Sheetal R. Dash <dash.sheetal13@gmail.com>
2018-08-14 02:37:27 +0100

    fixed issue with multi process runs

--- sniff-traffic.py
+++ sniff-traffic.py
@@ -8,31 +8,36 @@ logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s', level=logg
 logging.info('Connecting to Pulsar...')
 # Create a pulsar client instance with reference to the broker
 client = pulsar.Client('pulsar://localhost:6650')
-# Build a producer instance on a specific topic
-producer = client.create_producer(SNIFFER_TOPIC, max_pending_messages=100000)
+
 logging.info('Connected to Pulsar')
 def callBack(res, msg):
     print('Message published: %s'%res)
-
-def sniffPackets(packet):           # custom custom packet sniffer action method
-    if not (packet.haslayer(ICMP)):
-        if packet[IP].seq!=0:
-            pckt_src=packet[IP].src
-            pckt_dst=packet[IP].dst
-            pckt_ttl=packet[IP].ttl
-            print "IP Packet: %s is going to %s and has ttl value %s" % (pckt_src,pckt_dst,pckt_ttl)
-            producer.send_async(pckt_src + "|" + pckt_dst,callBack)
+def sniffPacketsWrapper(producer):
+    def sniffPackets(packet):  # custom custom packet sniffer action method
+        if not (packet.haslayer(ICMP)):
+            if packet[IP].seq != 0:
+                pckt_src = packet[IP].src
+                pckt_dst = packet[IP].dst
+                pckt_ttl = packet[IP].ttl
+                print
+                "IP Packet: %s is going to %s and has ttl value %s" % (pckt_src, pckt_dst, pckt_ttl)
+                producer.send_async(pckt_src + "|" + pckt_dst, callBack)
+                return
+        elif packet.haslayer(ICMP) and packet[ICMP].type != 0:
+            pckt_src = packet[IP].src
+            pckt_dst = packet[IP].dst
+            pckt_ttl = packet[IP].ttl
+            print
+            "IP Packet: %s is going to %s and has ttl value %s" % (pckt_src, pckt_dst, pckt_ttl)
+            # logging.info('Source|Target: %s ', pckt_src + "|" + pckt_dst)
+            producer.send_async(pckt_src + "|" + pckt_dst, callBack)
             return
-    elif packet.haslayer(ICMP) and packet[ICMP].type != 0:
-        pckt_src=packet[IP].src
-        pckt_dst=packet[IP].dst
-        pckt_ttl=packet[IP].ttl
-        print "IP Packet: %s is going to %s and has ttl value %s" % (pckt_src,pckt_dst,pckt_ttl)
-        #logging.info('Source|Target: %s ', pckt_src + "|" + pckt_dst)
-        producer.send_async(pckt_src + "|" + pckt_dst,callBack)
-        return
+    return sniffPackets
+
 def sniffPacketsSwitch(switch):
-    sniff(filter="ip",iface=switch,prn=sniffPackets)
+    # Build a producer instance on a specific topic
+    producer = client.create_producer(SNIFFER_TOPIC, max_pending_messages=100000)
+    sniff(filter="ip",iface=switch,prn=sniffPacketsWrapper(producer))
 
 def main():
 
